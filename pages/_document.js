5f7e105dba3c27ed9e25e23401e2eea3839075fa
lofigrammer <lofigrammer@gmail.com>
2022-12-04 02:11:50 +0400

    Add Lang Attribute

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => (
-  <Html>
+  <Html lang="en">
     <Head />
     <body className="bg-[#002b36] overflow-hidden">
       <Main />
