d724aeb025156d01569a57c744790e4146f883e8
lofigrammer <lofigrammer@gmail.com>
2022-12-04 02:08:34 +0400

    Add Title Tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
+import Head from "next/head";
 import Image from "next/image";
 import Github from "../components/icons/Github";
 import Instagram from "../components/icons/Instagram";
@@ -10,79 +11,85 @@ import fs from "fs";
 
 const Home = () => {
   return (
-    <div className="grid items-center justify-center grid-cols-[max-content_6rem]">
-      <div className="mb-40 p-6 pr-4 ">
-        <div className="flex items-center ">
-          <div className="mr-4 bg-violet p-6 shadow-card rounded-2xl flex hover:p-10">
-            <Image
-              className="rounded-full"
-              src={prefix + "/profile-picture.jpg"}
-              objectFit="cover"
-              alt="Avatar"
-              width={200}
-              height={200}
-            />
+    <>
+      <Head>
+        <title>Ankankhateseli</title>
+      </Head>
+
+      <div className="grid items-center justify-center grid-cols-[max-content_6rem]">
+        <div className="mb-40 p-6 pr-4 ">
+          <div className="flex items-center ">
+            <div className="mr-4 bg-violet p-6 shadow-card rounded-2xl flex hover:p-10">
+              <Image
+                className="rounded-full"
+                src={prefix + "/profile-picture.jpg"}
+                objectFit="cover"
+                alt="Avatar"
+                width={200}
+                height={200}
+              />
+            </div>
+            <div className="bg-violet p-6 rounded-2xl h-[calc(200px+3rem)]  shadow-card grid items-center hover:p-16">
+              <h2 className="font-fredoka-one  text-base3 text-center text-3xl">
+                Ankankhateseli
+              </h2>
+            </div>
           </div>
-          <div className="bg-violet p-6 rounded-2xl h-[calc(200px+3rem)]  shadow-card grid items-center hover:p-16">
-            <h2 className="font-fredoka-one  text-base3 text-center text-3xl">
-              Ankankhateseli
-            </h2>
+          <div className="grid mt-4 bg-violet p-6 rounded-2xl hover:py-10 shadow-card">
+            <div className="tracking-widest text-base3 font-fredoka-one text-center ">
+              <p>The Future Is Unpredictable</p>
+            </div>
           </div>
         </div>
-        <div className="grid mt-4 bg-violet p-6 rounded-2xl hover:py-10 shadow-card">
-          <div className="tracking-widest text-base3 font-fredoka-one text-center ">
-            <p>The Future Is Unpredictable</p>
+        <div className="mb-40 h-[21rem] flex flex-col justify-between items-center shadow-card bg-violet p-6 rounded-2xl hover:px-20">
+          <div>
+            <a
+              href="https://github.com/ankankhateseli/"
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              <Github />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/ankankhateseli/"
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              <Linkedin />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://twitter.com/ankankhateseli/"
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              <Twitter />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://t.me/ankankhateseli/"
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              <Telegram />
+            </a>
+          </div>
+          <div>
+            <a
+              href="http://instagram.com/lofigrammer/"
+              rel="noreferrer"
+              target={"_blank"}
+            >
+              <Instagram />
+            </a>
           </div>
         </div>
       </div>
-      <div className="mb-40 h-[21rem] flex flex-col justify-between items-center shadow-card bg-violet p-6 rounded-2xl hover:px-20">
-        <div>
-          <a
-            href="https://github.com/ankankhateseli/"
-            rel="noreferrer"
-            target={"_blank"}
-          >
-            <Github />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.linkedin.com/in/ankankhateseli/"
-            rel="noreferrer"
-            target={"_blank"}
-          >
-            <Linkedin />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://twitter.com/ankankhateseli/"
-            rel="noreferrer"
-            target={"_blank"}
-          >
-            <Twitter />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://t.me/ankankhateseli/"
-            rel="noreferrer"
-            target={"_blank"}
-          >
-            <Telegram />
-          </a>
-        </div>
-        <div>
-          <a
-            href="http://instagram.com/lofigrammer/"
-            rel="noreferrer"
-            target={"_blank"}
-          >
-            <Instagram />
-          </a>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 export async function getStaticProps() {
--- pages/works/index.jsx
+++ pages/works/index.jsx
@@ -1,4 +1,5 @@
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
+import Head from "next/head";
 import React, { useState } from "react";
 import Image from "next/image";
 import fs from "fs";
@@ -8,83 +9,88 @@ import Link from "next/link";
 
 const Works = ({ projects }) => {
   return (
-    <div className="grid  overflow-hidden items-center justify-center mobile:w-[calc(100vw_-_2.5rem)] laptop:w-9/12 m-auto h-[calc(100vh_-_11rem)]">
-      <div className="grid border-4 border-[#002b36] rounded-xl overflow-auto h-full ">
-        {projects.map(
-          ({ id, slug, title, link, content, workType, cover, tags }, i) => {
-            return (
-              <div key={id} className="mt-6  grid items-start ">
-                <div
-                  className={
-                    "laptop:flex mobile:grid mobile:gap-8 items-center " +
-                    (i % 2 == 0 ? "flex-row-reverse" : "")
-                  }
-                >
+    <>
+      <Head>
+        <title>Ankankhateseli - Works</title>
+      </Head>
+      <div className="grid  overflow-hidden items-center justify-center mobile:w-[calc(100vw_-_2.5rem)] laptop:w-9/12 m-auto h-[calc(100vh_-_11rem)]">
+        <div className="grid border-4 border-[#002b36] rounded-xl overflow-auto h-full ">
+          {projects.map(
+            ({ id, slug, title, link, content, workType, cover, tags }, i) => {
+              return (
+                <div key={id} className="mt-6  grid items-start ">
                   <div
                     className={
-                      "relative -z-10 border-violet border-2 rounded-2xl overflow-hidden laptop:w-2/3 h-96  mobile:w-full "
+                      "laptop:flex mobile:grid mobile:gap-8 items-center " +
+                      (i % 2 == 0 ? "flex-row-reverse" : "")
                     }
                   >
-                    {workType === "link" ? (
-                      <iframe
-                        src={link}
-                        className="bg-white"
-                        width="100%"
-                        height="100%"
-                        frameBorder="0"
-                        title={title}
-                      ></iframe>
-                    ) : (
-                      <Image
-                        src={prefix + "/" + cover}
-                        layout="fill"
-                        objectFit="contain"
-                        alt={title}
-                      />
-                    )}
-                  </div>
-                  <div
-                    className={
-                      "mobile:row-start-1	 mobile:h-auto laptop:h-96 p-6 text-center shadow-card  justify-center  text-5xl font-Roboto tracking-widest w-full bg-violet rounded-2xl flex items-start text-base3 laptop:" +
-                      (i % 2 == 0 ? "mr-6" : "ml-6")
-                    }
-                  >
-                    <div className="grid w-full">
-                      <div className="flex justify-between items-center  border-4 bg-[#002b36] border-[#002b36]  rounded-lg">
-                        <div className="text-2xl grid h-full w-full pl-4 items-center justify-start bg-violet rounded-lg">
-                          {title}
+                    <div
+                      className={
+                        "relative -z-10 border-violet border-2 rounded-2xl overflow-hidden laptop:w-2/3 h-96  mobile:w-full "
+                      }
+                    >
+                      {workType === "link" ? (
+                        <iframe
+                          src={link}
+                          className="bg-white"
+                          width="100%"
+                          height="100%"
+                          frameBorder="0"
+                          title={title}
+                        ></iframe>
+                      ) : (
+                        <Image
+                          src={prefix + "/" + cover}
+                          layout="fill"
+                          objectFit="contain"
+                          alt={title}
+                        />
+                      )}
+                    </div>
+                    <div
+                      className={
+                        "mobile:row-start-1	 mobile:h-auto laptop:h-96 p-6 text-center shadow-card  justify-center  text-5xl font-Roboto tracking-widest w-full bg-violet rounded-2xl flex items-start text-base3 laptop:" +
+                        (i % 2 == 0 ? "mr-6" : "ml-6")
+                      }
+                    >
+                      <div className="grid w-full">
+                        <div className="flex justify-between items-center  border-4 bg-[#002b36] border-[#002b36]  rounded-lg">
+                          <div className="text-2xl grid h-full w-full pl-4 items-center justify-start bg-violet rounded-lg">
+                            {title}
+                          </div>
+                          <div className="flex justify-center">
+                            <span className="hover:text-base3 hover:bg-transparent  cursor-pointer bg-[#002b36] text-sm flex  p-4   uppercase">
+                              <Link href={link}>See </Link>
+                            </span>
+                          </div>
                         </div>
-                        <div className="flex justify-center">
-                          <span className="hover:text-base3 hover:bg-transparent  cursor-pointer bg-[#002b36] text-sm flex  p-4   uppercase">
-                            <Link href={link}>See </Link>
-                          </span>
+                        <div className="mobile:mb-0 p-8  laptop:h-[15.7rem] mobile:h-auto border-[#002b36] border-4 rounded-lg my-6 overflow-auto">
+                          <p className="text-left text-base tracking-normal whitespace-pre-line">
+                            <span className="flex">
+                              {tags.split(",").map((i, j) => (
+                                <span
+                                  key={j}
+                                  className="mr-2 border-2 p-2 rounded-md"
+                                >
+                                  {" "}
+                                  {i}{" "}
+                                </span>
+                              ))}
+                            </span>
+                            {content}
+                          </p>
                         </div>
-                      </div>
-                      <div className="mobile:mb-0 p-8  laptop:h-[15.7rem] mobile:h-auto border-[#002b36] border-4 rounded-lg my-6 overflow-auto">
-                        <p className="text-left text-base tracking-normal whitespace-pre-line">
-                          <span className="flex">
-                            {tags.split(",").map((i, j) => (
-                              <span
-                                key={j}
-                                className="mr-2 border-2 p-2 rounded-md"
-                              >
-                                {" "}
-                                {i}{" "}
-                              </span>
-                            ))}
-                          </span>
-                          {content}
-                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          }
-        )}
+              );
+            }
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
