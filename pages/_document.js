3c01552361590f22d33e3ff73a8f775cd903a39b
lofigrammer <lofigrammer@gmail.com>
2022-12-04 02:18:36 +0400

    Add Meta Tags

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <meta name="description" content="Ankankhateseli Portfolio" />
+      <meta name="author" content="Ankankhateseli" />
+      <meta name="keywords" content="Developer, Portfolio, Ankankhateseli" />
+
+      <meta name="robots" content="index, follow" />
+    </Head>
     <body className="bg-[#002b36] overflow-hidden">
       <Main />
       <NextScript />
