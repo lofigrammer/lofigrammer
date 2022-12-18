a76db1053ed39624c0efce5d9dc8d95bab0a09a7
lofigrammer <lofigrammer@gmail.com>
2022-12-18 16:54:44 +0400

    Static Assets And Base Paths

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "";
-let basePath = "";
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+const assetPrefix = "/";
+const basePath = "/";
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
 module.exports = {
   async headers() {
     return [
